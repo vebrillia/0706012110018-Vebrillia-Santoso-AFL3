06bf7beb4b8ded44b6ccee040904e2051deae7d9
Vebrillia <vebrillias@gmail.com>
2023-04-11 08:21:50 +0700

    Initial Commit

---
+++ 0706012110018-Vebrillia Santoso-AFL3/_706012110018_Vebrillia_Santoso_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110018_Vebrillia_Santoso_AFL3App.swift
+//  0706012110018-Vebrillia Santoso-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110018_Vebrillia_Santoso_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
