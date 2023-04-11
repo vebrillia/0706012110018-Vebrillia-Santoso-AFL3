fe394925051cc26d0d9d6e272c91842fcc38e949
Vebrillia <vebrillias@gmail.com>
2023-04-11 09:21:20 +0700

    Section 6 - Compose the Detail View // Chapter 1 Done

--- 0706012110018-Vebrillia Santoso-AFL3/ContentView.swift
+++ 0706012110018-Vebrillia Santoso-AFL3/ContentView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
@@ -20,8 +28,18 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
+            
+            Spacer()
         }
     }
 }
