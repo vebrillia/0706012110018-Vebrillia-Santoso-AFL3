6a1790c86a7b08b27b5f8a421e1be261a62a7fd9
Vebrillia <vebrillias@gmail.com>
2023-04-11 08:39:58 +0700

    Section 2

--- 0706012110018-Vebrillia Santoso-AFL3/ContentView.swift
+++ 0706012110018-Vebrillia Santoso-AFL3/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
-        
+        Text("Turtle Rock")
+            .font(.title)
     }
 }
 
