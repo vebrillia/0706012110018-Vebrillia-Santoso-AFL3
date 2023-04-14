02c6dd60955e582ad822c731153596e8ebf5c46c
Vebrillia <vebrillias@gmail.com>
2023-04-14 09:35:09 +0700

    Section 6 - Set Up Navigation Between List and Detail

--- 0706012110018-Vebrillia Santoso-AFL3/Views/LandmarkList.swift
+++ 0706012110018-Vebrillia Santoso-AFL3/Views/LandmarkList.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        //Pass the model data's landmarks array to the List initializer
-        List(landmarks){ landmark in
-            //returning a LandmarkRow from the closure
-            LandmarkRow(landmark: landmark)
+        //Embed the dynamically generated list of landmarks
+        NavigationView{
+            //Pass the model data's landmarks array to the List initializer
+            List(landmarks){ landmark in
+                //Wrap the returned row 
+                NavigationLink{
+                    LandmarkDetail()
+                }label: {
+                    //returning a LandmarkRow from the closure
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            //Call the navigation Title
+            .navigationTitle("Landmarks")
         }
     }
 }
