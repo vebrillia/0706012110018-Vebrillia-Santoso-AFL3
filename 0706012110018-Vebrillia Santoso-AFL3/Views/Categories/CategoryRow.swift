07ff7fd4a5081a2f0d12f1bbec34c342a7f3bc87
Vebrillia <vebrillias@gmail.com>
2023-04-27 13:47:21 +0700

    Section 3 - Create a Category Row

---
+++ 0706012110018-Vebrillia Santoso-AFL3/Views/Categories/CategoryRow.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryRow.swift
+//  0706012110018-Vebrillia Santoso-AFL3
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+        var items: [Landmark]
+
+    var body: some View {
+        //display name of category
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+        static var previews: some View {
+            CategoryRow(
+                categoryName: landmarks[0].category.rawValue,
+                items: Array(landmarks.prefix(4))
+            )
+    }
+}
